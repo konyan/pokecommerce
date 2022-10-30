ae23a74d05585785b586ba6e40c717e0eb6b80a9
ko nyan <konyan@kos-MacBook-Pro.local>
2022-10-30 21:47:52 +0700

    first commit

--- apps/pokemon-e2e/src/integration/app.spec.ts
+++ apps/pokemon-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('enduser', () => {
+describe('pokemon', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('enduser', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome enduser');
+    getGreeting().contains('Welcome pokemon');
   });
 });
