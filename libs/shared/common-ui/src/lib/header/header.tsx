697e6b7c36818dfb387e7bb951a6c3074e5122cc
ko nyan <konyan@kos-MacBook-Pro.local>
2022-10-30 22:02:35 +0700

    remove unused code

--- libs/shared/common-ui/src/lib/header/header.tsx
+++ libs/shared/common-ui/src/lib/header/header.tsx
@@ -2,30 +2,29 @@ import styles from './header.module.scss';
 
 /* eslint-disable-next-line */
 export interface HeaderProps {
-  logo: string,
+  logo: string;
   texts: {
-    explore: string,
-    all: string,
-    appareal: string,
-    motion: string,
-    about: string,
-    help: string,
-    cart: string,
-    signIn: string,
-  },
-  isLoggedin: boolean,
-  notificationBox: React.Component,
-  notificationCount: number,
-  userInfoBox: React.Component,
+    explore: string;
+    all: string;
+    appareal: string;
+    motion: string;
+    about: string;
+    help: string;
+    cart: string;
+    signIn: string;
+  };
+  isLoggedin: boolean;
+  notificationBox: React.Component;
+  notificationCount: number;
+  userInfoBox: React.Component;
 }
 
 export function Header(props: HeaderProps) {
   return (
     <header className={styles['container']}>
       <div>
-        <h1>Welcome to Header!</h1>
+        <h1>TCG Marketplace</h1>
       </div>
-
     </header>
   );
 }
